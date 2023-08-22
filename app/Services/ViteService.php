2479977f286963da32d3cd48da57eddc32fb1857
Marcel Hric <marcelkohric@gmail.com>
2023-08-22 22:21:22 +0200

    minor refactor

--- app/Services/ViteService.php
+++ app/Services/ViteService.php
@@ -32,7 +32,7 @@ class ViteService
 	public function getAsset(string $entrypoint): string
 	{
 		// Handle dev state
-		if ($this->isEnabled()) {
+		if (!$this->isProduction()) {
 			return $this->viteServer . '/' . $entrypoint;
 		}
 
@@ -48,40 +48,6 @@ class ViteService
 	}
 
 	/**
-	 * @return array<string>
-	 * @throws \Nette\Utils\JsonException
-	 */
-	public function getCssAssets(string $entrypoint): array
-	{
-		// Handle dev state
-		if ($this->isEnabled()) {
-			return [];
-		}
-
-		// Handle production state
-		if (!file_exists($this->manifestFile)) {
-			throw new Exception('Missing manifest file: ' . $this->manifestFile);
-		}
-
-		$assets = [];
-		$manifest = Json::decode(FileSystem::read($this->manifestFile), true);
-		foreach ($manifest[$entrypoint]['css'] ?? [] as $asset) {
-			$assets[] = self::BaseUrl . $asset;
-		}
-
-		return $assets;
-	}
-
-	public function isEnabled(): bool
-	{
-		if (!$this->productionMode) {
-			return true;
-		}
-
-		return false;
-	}
-
-	/**
 	 * @return array<\Nette\Utils\Html>
 	 * @throws \Nette\Utils\JsonException
 	 */
@@ -91,7 +57,7 @@ class ViteService
 		$styles = $this->getCssAssets($entrypoint);
 
 		$tags = [];
-		if ($this->isEnabled()) {
+		if (!$this->isProduction()) {
 			$tags[] = Html::el('script')->type('module')->src($this->viteServer . '/' . '@vite/client');
 		}
 
@@ -114,5 +80,36 @@ class ViteService
 		foreach ($this->getTags($entrypoint) as $tag) {
 			echo $tag;
 		}
+	}
+
+	public function isProduction(): bool
+	{
+		return $this->productionMode;
+	}
+
+	/**
+	 * @return array<string>
+	 * @throws \Nette\Utils\JsonException
+	 * @throws Exception
+	 */
+	private function getCssAssets(string $entrypoint): array
+	{
+		// Handle dev state
+		if (!$this->isProduction()) {
+			return [];
+		}
+
+		// Handle production state
+		if (!file_exists($this->manifestFile)) {
+			throw new Exception('Missing manifest file: ' . $this->manifestFile);
+		}
+
+		$assets = [];
+		$manifest = Json::decode(FileSystem::read($this->manifestFile), true);
+		foreach ($manifest[$entrypoint]['css'] ?? [] as $asset) {
+			$assets[] = self::BaseUrl . $asset;
+		}
+
+		return $assets;
 	}
 }
